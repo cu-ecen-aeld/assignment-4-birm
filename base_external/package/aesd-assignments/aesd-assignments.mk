6771b1805de35b7f6905d68868bff236d30538bc
Ryan Birmingham <birm@rbirm.us>
2023-03-05 01:20:15 -0500

    update makefile upstream ref

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '55c06aaf19b585f753b2aaba06c2bd01334a2127'
+AESD_ASSIGNMENTS_VERSION = 'edfd9faed28ac2323c39d44d8ef44f0b314de708'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
